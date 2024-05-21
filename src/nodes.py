d30ec7cb0495e10c82c9d3a4d1a8d4733cedbca4
federico Lubbert <federico.lubbert@gmail.com>
2024-05-21 10:24:41 +0200

    formatted

--- src/nodes.py
+++ src/nodes.py
@@ -1,8 +1,8 @@
 import os
 import time
-
 from langchain_community.agent_toolkits import GmailToolkit
 from langchain_community.tools.gmail.search import GmailSearch
+
 
 class Nodes():
 	def __init__(self):
@@ -45,4 +45,3 @@ class Nodes():
 		else:
 			print("## New emails")
 			return "continue"
-

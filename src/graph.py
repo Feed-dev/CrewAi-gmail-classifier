fa3ca6b6e883f4fc29140a9a68aab1266c349fb0
federico Lubbert <federico.lubbert@gmail.com>
2024-05-22 09:22:05 +0200

    integrated new nodes

--- src/graph.py
+++ src/graph.py
@@ -15,6 +15,9 @@ class WorkFlow():
 		workflow.add_node("check_new_emails", nodes.check_email)
 		workflow.add_node("wait_next_run", nodes.wait_next_run)
 		workflow.add_node("draft_responses", EmailFilterCrew().kickoff)
+		workflow.add_node("categorize_complaints", nodes.categorize_complaints)
+		workflow.add_node("summarize_complaints", nodes.summarize_complaints)
+		workflow.add_node("save_summaries", nodes.save_summaries)
 
 		workflow.set_entry_point("check_new_emails")
 		workflow.add_conditional_edges(
@@ -25,6 +28,9 @@ class WorkFlow():
 					"end": 'wait_next_run'
 				}
 		)
-		workflow.add_edge('draft_responses', 'wait_next_run')
+		workflow.add_edge('draft_responses', 'categorize_complaints')
+		workflow.add_edge('categorize_complaints', 'summarize_complaints')
+		workflow.add_edge('summarize_complaints', 'save_summaries')
+		workflow.add_edge('save_summaries', 'wait_next_run')
 		workflow.add_edge('wait_next_run', 'check_new_emails')
 		self.app = workflow.compile()

ce0d4ad2fc46c687952ca1e8b9a055194f6cec1a
federico Lubbert <federico.lubbert@gmail.com>
2024-05-28 20:17:41 +0200

    updated main with initial state

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from src.graph import WorkFlow
 
-app = WorkFlow().app
-app.invoke({})
+if __name__ == "__main__":
+    app = WorkFlow().app
+    initial_state = {
+        "checked_emails_ids": [],
+        "emails": [],
+        "action_required_emails": {},
+        "complaint_emails": [],
+        "summaries": []
+    }
+    app.invoke(initial_state)
